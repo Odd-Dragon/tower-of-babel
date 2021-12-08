aee187c4950450dc73b2ce08f981b88d4abc4089
Kalina Warner <kalinamosher7@gmail.com>
2021-12-08 09:47:45 -0700

    removed pygame services

--- genie/services/__init__.py
+++ genie/services/__init__.py
@@ -1,9 +1,3 @@
-from .pygame_services.PygameAudioService import PygameAudioService
-from .pygame_services.PygameScreenService import PygameScreenService
-from .pygame_services.PygameKeyboardService import PygameKeyboardService
-from .pygame_services.PygameMouseService import PygameMouseService
-from .pygame_services.PygamePhysicsService import PygamePhysicsService
-
 from .raylib_services.RaylibAudioService import RaylibAudioService
 from .raylib_services.RaylibScreenService import RaylibScreenService
 from .raylib_services.RaylibKeyboardService import RaylibKeyboardService
@@ -15,12 +9,6 @@ from .constants import mouse
 from .constants import colors
 
 __all__ = [
-    'PygameAudioService',
-    'PygameScreenService',
-    'PygameKeyboardService',
-    'PygameMouseService',
-    'PygamePhysicsService',
-
     'RaylibAudioService',
     'RaylibScreenService',
     'RaylibKeyboardService',
