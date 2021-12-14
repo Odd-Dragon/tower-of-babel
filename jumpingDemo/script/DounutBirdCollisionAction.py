c55d764be05b0aca22061c25c80e7bfd0a610f5f
Landon Lee <lturtlesrock@gmail.com>
2021-12-14 14:10:17 -0700

    Update DounutBirdCollisionAction.py

--- jumpingDemo/script/DounutBirdCollisionAction.py
+++ jumpingDemo/script/DounutBirdCollisionAction.py
@@ -10,10 +10,11 @@ class DounutBirdCollisionAction(Action):
     def execute(self, actors, actions, clock, callback):
         birds = actors.get_actors("birds")
         dounuts = actors.get_actors("dounuts")
+        player = actors.get_first_actor("player")
         for bird in birds:
             for dounut in dounuts:
                 if self._physics_service.check_collision(bird, dounut):
-                    feather = Feather("resources/feather.png", 30, 30, bird.get_x(), bird.get_y(), 0, 1.5, 0, 0, False)
+                    feather = Feather("resources/feather.png", 30, 30, bird.get_x(), bird.get_y(), 0, player.get_difficulty(), 0, 0, False)
                     actors.remove_actor("birds", bird)
                     actors.remove_actor("dounuts", dounut)
                     actors.add_actor("feathers", feather)
